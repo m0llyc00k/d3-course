d55760b07944580894b871159dbd556c78708456
m0llyc00k <mollyc628@gmail.com>
2022-06-13 14:40:31 -0400

    reloaded data with topojson, made map dynamic, moved title & subtitle out of onMount'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,11 @@
 	/** @type {import('@sveltejs/kit').Load} */
 	export async function load({ params, fetch, session, stuff }) {
 		return {
-			props: {
-				title: 'Mapping Vulnerability in the Opioid Crisis',
-				subtitle:
-					'Using attribute clustering to determine the risk status of US counties based on opioid pill distribution, overdose death rates, and the availability of medication-assisted treatment (MAT).'
-			}
+			// props: {
+			// 	title: 'Mapping Vulnerability in the Opioid Crisis',
+			// 	subtitle:
+			// 		'Using attribute clustering to determine the risk status of US counties based on opioid pill distribution, overdose death rates, and the availability of medication-assisted treatment (MAT).'
+			// }
 		};
 	}
 </script>
@@ -16,20 +16,25 @@
 	import Map from '$lib/components/charts/Map.svelte';
 	import { onMount } from 'svelte';
 	import * as d3 from 'd3';
-	export let title = '';
-	export let subtitle = '';
+	// export let title = '';
+	// export let subtitle = '';
+
+	let title = 'Mapping Vulnerability in the Opioid Crisis';
+	let subtitle =
+		'Using attribute clustering to determine the risk status of US counties based on opioid pill distribution, overdose death rates, and the availability of medication-assisted treatment (MAT).';
 
 	const mapURL =
 		'https://raw.githubusercontent.com/m0llyc00k/Summer2022/main/county-state-analysis.json';
 
-	let counties = [];
+	// let counties = [];
+	export let geojson = [];
 
 	// get data
-	onMount(async () => {
-		await d3.json(mapURL).then((geojson) => {
-			counties = geojson.features;
-		});
-	});
+	// onMount(async () => {
+	// 	await d3.json(mapURL).then((geojson) => {
+	// 		counties = geojson.features;
+	// 	});
+	// });
 </script>
 
 <article class="prose-headings:text-primary pt-4 content-well text-center">
@@ -37,5 +42,5 @@
 		<h1 class="sm:text-4xl font-bold mx-auto">{title}</h1>
 		<h2 class="sm:text-md mx-auto pt-2 pb-0 max-w-3xl">{subtitle}</h2>
 	</header>
-	<Map {counties} />
+	<Map {geojson} />
 </article>
